8c307b404312fa9c96c170f5921b957666b286d1
jmshreyas882000-ux <jmshreyas882000@gmail.com>
2026-05-06 14:56:23 +0530

    Add login and chatAI functions to script.js

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ function sendData() {
   .then(res => res.json())
   .then(data => alert("Submitted Successfully"));
 }
+function login() {
+  fetch("http://127.0.0.1:5000/login", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+      username: "admin",
+      password: "admin"
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    localStorage.setItem("token", data.token);
+    alert("Login Success");
+  });
+}
+
+function chatAI() {
+  const msg = prompt("Ask AI:");
+
+  fetch("http://127.0.0.1:5000/chat", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({message: msg})
+  })
+  .then(res => res.json())
+  .then(data => alert(data.reply));
+}
